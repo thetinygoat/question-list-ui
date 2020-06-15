67e03915e1f068018321e217a8f669bd2ea015ed
Sachin Saini <iamsainisachin@gmail.com>
2020-06-16 04:55:16 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDpTntWIWJCG-oMvm9MgystgdsO4DLdwoE',
-  authDomain: 'question-list-a4a77.firebaseapp.com',
-  databaseURL: 'https://question-list-a4a77.firebaseio.com',
-  projectId: 'question-list-a4a77',
-  storageBucket: 'question-list-a4a77.appspot.com',
-  messagingSenderId: '728809510469',
-  appId: '1:728809510469:web:a5052cdc624428f720352c',
+//config here
 };
 
 const app = firebase.initializeApp(firebaseConfig);
